80101606817f57e29295616513bca7530f2b9a21
elyazid_flutter <ely.alaoui522@gmail.com>
2026-01-03 12:53:52 +0100

    Initial commit

---
+++ tp9-1-jms-activemq-main/jms-activemq-queue-example/src/main/java/ma/formations/jms/Main.java
@@ -0,0 +1,28 @@
+package ma.formations.jms;
+
+import jakarta.jms.JMSException;
+
+public class Main {
+    public static void main(String[] args) {
+        String brockerUrl = "tcp://localhost:61616";
+        String destinationName = "queue1";
+
+        JmsQueueProducer producer = new JmsQueueProducer(destinationName, brockerUrl);
+        JmsQueueConsumer consumer = new JmsQueueConsumer(destinationName, brockerUrl);
+
+        try {
+            producer.send("premier msg");
+            consumer.receive();
+
+            producer.send("deuxième msg");
+            consumer.receive();
+
+            producer.send("...");
+            consumer.receive();
+
+            producer.send("other message"); // volontairement pas consommé (comme TP)
+        } catch (JMSException e) {
+            e.printStackTrace();
+        }
+    }
+}
